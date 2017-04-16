3aeb0263ba92e1f0d74388b63f77f864875367e7
Rafael Faria <rafaelr.faria@yahoo.com.br>
2017-04-16 10:26:21 -0300

    bdd initial test passed

--- features/home.feature
+++ features/home.feature
@@ -1,7 +1,7 @@
 Feature: Test open page
-  I will test the open page of app
+  I will test open app page
 
   Scenario: Try open the initial page
     Given the initial page
-    And I have the content table in body
-    Then I should have a title "Projeto Crud - localStorage" on page
+    And there is the content table in body
+    Then I should have a title "Projeto Crud - LocalStorage" on page
--- features/step_definitions/home.js
+++ features/step_definitions/home.js
@@ -2,24 +2,30 @@
 var seleniumWebdriver = require('selenium-webdriver');
 
 module.exports = function () {
-    this.Given(/^the initial page$/, function(done) {
-        helpers.loadPage('http://mywebsite.dev:3001/')
-        .then(
-            done()
-        );
+    this.Given(/^the initial page$/, function() {
+        return helpers.loadPage('http://mywebsite.dev:3001/')
+        .then(function () {
+               return driver.wait(until.elementsLocated(by.css('body')),30000).then(function(){
+
+                   // return the promise of an element to the following then.
+                   return driver.findElements(by.css('body'));
+               })
+        });
 
     });
 
-    this.When(/^I have the content table in body$/, function (done) {
-        driver.findElement(by.id("txtFullname")).then(function(element) {
-            //expect(elements.length).to.not.equal(0);
-            done();
+    this.When(/^there is the content table in body$/, function () {
+        driver.wait(until.elementsLocated(by.id("tablerows")), 20000).then(function(element) {
+            return driver.findElements(by.css('#tablerows'));
         });
     });
 
     this.Then(/^I should have a title "([^"]*)" on page$/, function (text) {
-        var xpath = "//*[contains(text(),'" + text + "')]";
-        var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
-        return this.driver.wait(condition, 5000);
+        driver.findElement(by.css('h2.title')).then(function(h2) {
+            h2.getText().then(function(content){
+                expect(content).to.be.a('string');
+                expect(content).to.equal(text);
+            })
+        })
     });
 };
